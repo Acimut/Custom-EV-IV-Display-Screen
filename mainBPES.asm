479e7d926adc034110ddae5f3ae4b57e7427979b
Acimut <79547805+Acimut@users.noreply.github.com>
2022-08-21 14:59:26 -0500

    Esmeralda

--- mainBPES.asm
+++ mainBPES.asm
@@ -4,9 +4,9 @@
 //---------------------------------------------------
 
     //summary screen
-    //081c0510 l 000000f4 Task_HandleInput
-    .org 0x081c0510
-.area 0xF4, 0xFF
+//081C0130 l 000000f4 Task_HandleInput
+.org 0x081C0130
+.area 0xF4//, 0xFF
     push    {r4,lr}
     ldr     r3, =Task_HandleInput|1
     bl      bx_r3_Task_HandleInput
@@ -16,9 +16,14 @@ bx_r3_Task_HandleInput:
 .pool
 .endarea
 
-    //081c2afc l 0000013c PutPageWindowTilemaps
-    .org 0x081c2afc
-.area 0x13c, 0xFF
+/*
+b/t 0x081c271C
+ok
+
+*/
+//081c271C l 0000013c PutPageWindowTilemaps
+.org 0x081c271C
+.area 0x13c//, 0xFF
     push    {r4,lr}
     ldr     r3, =PutPageWindowTilemaps|1
     bl      bx_r3_PutPageWindowTilemaps
@@ -28,9 +33,9 @@ bx_r3_PutPageWindowTilemaps:
 .pool
 .endarea
 
-    //081c2c38 l 000000f4 ClearPageWindowTilemaps
-    .org 0x081c2c38
-.area 0x0f4, 0xFF
+//081c2858 l 000000f4 ClearPageWindowTilemaps
+.org 0x081c2858
+.area 0x0f4//, 0xFF
     push    {r4,lr}
     ldr     r3, =ClearPageWindowTilemaps|1
     bl      bx_r3_ClearPageWindowTilemaps
